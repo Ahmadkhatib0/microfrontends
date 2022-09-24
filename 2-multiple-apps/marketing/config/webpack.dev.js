d5b3b12a1544c759f92e4e1917dcbebb3f235115
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-09-24 13:12:04 +0300

    inititalize the second project, setup webpack in the marketing child react app

---
+++ 2-multiple-apps/marketing/config/webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge'); //merges different webpack config objects
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: { index: 'index.html' },
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+};
+
+module.exports = merge(commonConfig, devConfig); //devConfig takes priority
