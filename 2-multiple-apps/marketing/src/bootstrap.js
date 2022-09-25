6f3109b076adbcca07ab3dfeb3c140252b5cfe90
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-09-25 17:32:17 +0300

    initialEntries to fix clicking twice to reach out auth or marketing

--- 2-multiple-apps/marketing/src/bootstrap.js
+++ 2-multiple-apps/marketing/src/bootstrap.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (ele, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (ele, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
   if (onNavigate) history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, ele);
